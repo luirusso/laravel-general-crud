46d6256aafab64a287d57ac86fb7ddfb959759e5
luirusso <luirusso_@hotmail.it>
2022-01-28 15:30:20 +0100

    same as previous commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-center py-4">
+    Powered by Boolean - All rights reserver
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// HOMEPAGE
+Route::get('/', 'HomeController@index')->name('home');
